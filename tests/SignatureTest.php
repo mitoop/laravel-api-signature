3c896e963502344b2ef6009b311535f4306d31c3
zhuzhichao <me@zhuzhichao.com>
2019-01-17 10:53:21 +0800

    Add BaseTestCase class and phpunit xml.

---
+++ tests/SignatureTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests;
+
+use Mitoop\ApiSignature\Signature;
+
+class SignatureTest extends TestCase
+{
+
+    public function testValidSign()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function testSign()
+    {
+        $this->assertTrue(false);
+    }
+}
