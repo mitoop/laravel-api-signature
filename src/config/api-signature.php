2664d75ea9c24410b36ee3f00d5221ae7ed92a2c
zhuzhichao <me@zhuzhichao.com>
2019-01-18 10:21:27 +0800

    Update config.

--- src/config/api-signature.php
+++ src/config/api-signature.php
@@ -1,33 +1,36 @@
 <?php
 
 return [
-    'default' => 'client1',
+    'default' => 'main',
 
     'clients' => [
-        'client1' => [
-            'app_id'         => 'app id', // app id 必填
-            'app_secret'     => 'app secret', //app secret 必填
-            'scheme'         => '', // https or https 非必填
-            'host'           => '', // www.google.com 必填
-            'ip'             => '', // 192.169.11.11 非必填
-            'port'           => '', // 80 非必填
-            'https_cert_pem' => 'cert.pem path', // https请求时 证书路径 true 调用系统证书 false 禁用验证
+        'main' => [
+            'app_id'         => env('SIGN_main_APP_ID', 'app id'), // app id 必填
+            'app_secret'     => env('SIGN_main_APP_SECRET', 'app secret'), //app secret 必填
+            'scheme'         => env('SIGN_main_SCHEME', ''), // https or https 非必填
+            'host'           => env('SIGN_main_HOST', ''), // www.google.com 必填
+            'ip'             => env('SIGN_main_IP', ''), // 192.169.11.11 非必填
+            'port'           => env('SIGN_main_PORT', ''), // 80 非必填
+            'https_cert_pem' => env('SIGN_main_HTTPS_CERT_PEM', 'cert.pem path'), // https请求时 证书路径 true 调用系统证书 false 禁用验证
         ],
 
         // another client
-        'client2' => [
-            'app_id'         => 'app id', // app id 必填
-            'app_secret'     => 'app secret', //app secret 必填
-            'scheme'         => '', // https or https 非必填
-            'host'           => '', // www.google.com 必填
-            'ip'             => '', // 192.169.11.11 非必填
-            'port'           => '', // 80 非必填
-            'https_cert_pem' => 'cert.pem path', // https请求时 证书路径 true 调用系统证书 false 禁用验证
+        'minor' => [
+            'app_id'         => env('SIGN_MINOR_APP_ID', 'app id'), // app id 必填
+            'app_secret'     => env('SIGN_MINOR_APP_SECRET', 'app secret'), //app secret 必填
+            'scheme'         => env('SIGN_MINOR_SCHEME', ''), // https or https 非必填
+            'host'           => env('SIGN_MINOR_HOST', ''), // www.google.com 必填
+            'ip'             => env('SIGN_MINOR_IP', ''), // 192.169.11.11 非必填
+            'port'           => env('SIGN_MINOR_PORT', ''), // 80 非必填
+            'https_cert_pem' => env('SIGN_MINOR_HTTPS_CERT_PEM', 'cert.pem path'), // https请求时 证书路径 true 调用系统证书 false 禁用验证
         ],
     ],
 
-    'identity'       => '', // 标识当前系统的身份 用户生成nonce request id时作为前缀
+    'identity'       => '', // 标识当前系统的身份 用户生成 nonce request id 时作为前缀
+
     'logger_handler' => function ($message, array $data) {
-        \Log::info($message, $data);
-    },
+        if(config('app.debug')) {
+            \Log::info($message, $data);
+        }
+     },
 ];
